162774e4a8b1ef363c78ba49d5b769043f4499c0
mrhpn <mr.htetphyonaing@gmail.com>
2022-08-09 15:14:12 +0630

    pssudo elements

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -13,15 +13,30 @@ export const Base = styled.button`
   font-family: Helvetica Neue, ui-sans-serif, system-ui, -apple-system,
     BlinkMacSystemFont, Segoe UI, Roboto, Arial, Noto Sans, sans-serif,
     Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+
+  &:hover {
+    background: ${({ variant }) =>
+      variant === 'outline' ? '#fff828' : '#fff'};
+    cursor: pointer;
+  }
 `;
 
 export const Featured = styled(Base)`
   color: white;
+  border: 3px #000 solid;
   background: rgb(63, 94, 251);
   background: radial-gradient(
     circle,
     rgba(63, 94, 251, 1) 0%,
     rgba(252, 70, 107, 1) 100%
   );
-  border: 3px #000 solid;
+
+  &:hover {
+    background: rgb(63, 94, 251);
+    background: radial-gradient(
+      circle,
+      rgba(252, 70, 107, 1) 100%,
+      rgba(63, 94, 251, 1) 0%
+    );
+  }
 `;
